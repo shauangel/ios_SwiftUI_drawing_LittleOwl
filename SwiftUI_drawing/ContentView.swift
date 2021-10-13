d9788b03eba2df78b2d688764c08795cb888f1ce
Angel Shau <shauangel2000@gmail.com>
2021-10-13 14:35:26 +0800

    Initial Commit

---
+++ SwiftUI_drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_drawing
+//
+//  Created by 邵安祺 on 2021/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_drawing/SwiftUI_drawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_drawingApp.swift
+//  SwiftUI_drawing
+//
+//  Created by 邵安祺 on 2021/10/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_drawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
